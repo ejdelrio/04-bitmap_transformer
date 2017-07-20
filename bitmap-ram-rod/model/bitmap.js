b34a5546842242fc5b9037f6a38d84957b4e7d13
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-20 14:33:55 -0700

    Modified Bitmap constructor to include color table

--- bitmap-ram-rod/model/bitmap.js
+++ bitmap-ram-rod/model/bitmap.js
@@ -12,9 +12,19 @@ fs.readFile(`${__dirname}/../assets/palette-bitmap.bmp`, (err, asset) => {
 const Bitmap = function(buffer) {
   this.width = buffer.readUInt32LE(22);
   this.height = buffer.readUInt32LE(18);
-  this.pixelOffset = buffer.readUInt32LE(10);
+  this.pixelTableStart = buffer.readUInt32LE(10);
   this.size = buffer.readUInt32LE(2);
   this.type = buffer.toString('utf-8', 0, 2);
   this.bitsPerPixel = buffer.readUInt32LE(28);
   this.headerSize = buffer.readUInt32LE(14);
+
+  this.bitsPerPixel < 16 ?
+  this.colorTableStartPoint = this.headerSize + 14 :
+  this.pixelTableStart = this.headerSize + 14 ;
+
+  this.pixelTableEnd = buffer.length;
+  this.colorTableStartPoint ?
+  this.colorTableEndPoint = this.pixelTableStart:
+  null;
+  this.buffer = buffer;
 };
