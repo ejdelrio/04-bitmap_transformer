362287ea361888124354dab983ae5dd11a47a5e1
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-20 14:06:39 -0700

    Modifying bitmap object constructor

--- bitmap-ram-rod/model/bitmap.js
+++ bitmap-ram-rod/model/bitmap.js
@@ -6,13 +6,15 @@ var bitmap;
 fs.readFile(`${__dirname}/../assets/palette-bitmap.bmp`, (err, asset) => {
   if(err) console.error(err);
   bitmap = new Bitmap(asset);
+  console.log(bitmap);
 });
 
 const Bitmap = function(buffer) {
   this.width = buffer.readUInt32LE(22);
   this.height = buffer.readUInt32LE(18);
-  this.pixelOffset = buffer.readUInt32LE(18);
+  this.pixelOffset = buffer.readUInt32LE(10);
   this.size = buffer.readUInt32LE(2);
   this.type = buffer.toString('utf-8', 0, 2);
-
+  this.bitsPerPixel = buffer.readUInt32LE(28);
+  this.headerSize = buffer.readUInt32LE(14);
 };
