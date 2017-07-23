d7d83f788ca69546f0c918a44082306d7a972dca
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-23 10:34:07 -0700

    Succesfully plugged in transformation module and completed 4 transformations

--- bitmap-ram-rod/model/bitmap.js
+++ bitmap-ram-rod/model/bitmap.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const transform = require('../lib/transformer.js');
 
 
-module.exports = (path) => {
+module.exports = (path, callback, newFileName) => {
 
   const Bitmap = function(buffer) {
     this.type = buffer.toString('utf-8', 0, 2);
@@ -17,10 +17,10 @@ module.exports = (path) => {
     this.pixelTableStart = this.headerSize + 14 ;
 
     this.pixelTableEnd = buffer.length;
+
     this.colorTableStartPoint ?
     this.colorTableEndPoint = this.pixelTableStart:
     null;
-    this.buffer = buffer;
 
     this.transformedBMP = new TransformedBMP(buffer, this);
 
@@ -28,7 +28,7 @@ module.exports = (path) => {
 
   const TransformedBMP = function(asset, parent) {
     this.header = asset.slice(0, 14);
-    this.DIB = asset.slice(14, parent.headerSize);
+    this.DIB = asset.slice(14, parent.headerSize + 14);
 
     if(parent.colorTableStartPoint) this.colorPalette = asset.slice(14 + parent.headerSize, parent.pixelTableStart);
     this.pixelTable = asset.slice(parent.pixelTableStart);
@@ -45,7 +45,8 @@ module.exports = (path) => {
 
   TransformedBMP.prototype.newFile = function(fileName) {
     let newBuffer = this.compileBuffer();
-    fs.writeFile(`../assets/${fileName}.bmp`, (err) => {
+    console.log(newBuffer.length);
+    fs.writeFile(`../assets/${fileName}.bmp`, newBuffer, (err) => {
       if(err) console.error(err);
       console.log('verifier#: ' + newBuffer.readUInt32LE(58));
     });
@@ -55,15 +56,16 @@ module.exports = (path) => {
     if(err) console.error(err);
 
     let bitmap = new Bitmap(asset);
-    transform.blueShift(bitmap);
-    bitmap.transformedBMP.newFile('red');
+    callback(bitmap);
+    bitmap.transformedBMP.newFile(newFileName);
   });
 
 
 
 };
 
-module.exports(`${__dirname}/../assets/palette-bitmap.bmp`);
+module.exports('../assets/palette-bitmap.bmp', transform.blackOut, 'black');
+
 
 //bitmap.readUInt8(1078)
 //Used to get color palette index
