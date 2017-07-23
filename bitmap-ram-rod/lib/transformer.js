8ac8a887c2f482ba15f6bd717718871d03a7fe2f
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-22 19:46:57 -0700

    Created color phases Removes all reds,  greens or etc...

--- bitmap-ram-rod/lib/transformer.js
+++ bitmap-ram-rod/lib/transformer.js
@@ -6,35 +6,35 @@ const bitmap = require('../model/bitmap.js');
 transform.modify = (buffer, blueCallback, greenCallback, redCallback) => {
   let newColors = buffer.transformedBMP.colorPalette;
   for (let i = 7; i < newColors.length; i++) {
-    blueCallback(newColors[i - 7]);
-    blueCallback(newColors[i - 6]);
-    greenCallback(newColors[i - 5]);
-    greenCallback(newColors[i - 4]);
-    redCallback(newColors[i - 3]);
-    redCallback(newColors[i - 2]);
+    newColors[i - 7] = blueCallback(newColors[i - 7]);
+    newColors[i - 6] = blueCallback(newColors[i - 6]);
+    newColors[i - 5] = greenCallback(newColors[i - 5]);
+    newColors[i - 4] = greenCallback(newColors[i - 4]);
+    newColors[i - 3] = redCallback(newColors[i - 3]);
+    newColors[i - 2] = redCallback(newColors[i - 2]);
   }
 };
 
 transform.redShift = function(buffer) {
   this.modify(buffer,
     hexBlue => hexBlue,
-    hexGreen => hexGreen = '0',
-    hexRed => hexRed = '0'
+    () => '0',
+    () => '0'
   );
 };
 
 transform.blueShift = function(buffer) {
   this.modify(buffer,
-    hexBlue => hexBlue = '0',
-    hexGreen => hexGreen = '0',
+    () => '0',
+    () => '0',
     hexRed => hexRed
   );
 };
 
 transform.greenShift = function(buffer) {
   this.modify(buffer,
-    hexBlue => hexBlue = '0',
+    () => '0',
     hexGreen => hexGreen,
-    hexRed => hexRed = '0'
+    () => '0'
   );
 };
