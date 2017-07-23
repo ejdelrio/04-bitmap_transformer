d1b9c6ea024d78d6d3fcfed6eab9d3bfea7989e3
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-22 20:00:17 -0700

    Made revisions to object constructors.
    Fixed errors in the values of said object

--- bitmap-ram-rod/model/bitmap.js
+++ bitmap-ram-rod/model/bitmap.js
@@ -6,41 +6,40 @@ const transform = require('../lib/transformer.js')
 
 module.exports = (path, callback) => {
 
+  const Bitmap = function(buffer) {
+    this.type = buffer.toString('utf-8', 0, 2);
+    this.width = buffer.readUInt16LE(22);
+    this.height = buffer.readUInt16LE(18);
+    this.size = buffer.readUInt16LE(2);
+    this.pixelTableStart = buffer.readUInt16LE(10);
+    this.bitsPerPixel = buffer.readUInt16LE(28);
+    this.headerSize = buffer.readUInt16LE(14);
+
+    this.bitsPerPixel < 16 ?
+    this.colorTableStartPoint = this.headerSize + 14 :
+    this.pixelTableStart = this.headerSize + 14 ;
+
+    this.pixelTableEnd = buffer.length;
+    this.colorTableStartPoint ?
+    this.colorTableEndPoint = this.pixelTableStart:
+    null;
+    this.buffer = buffer;
+
+    this.transformedBMP = new TransformedBMP(buffer, this);
+
+  };
+
+  function TransformedBMP(asset, parent) {
+    this.header = asset.slice(0, 14);
+    this.DIB = asset.slice(14, 54);
+    if(parent.colorTableStartPoint) this.colorPalette = asset.slice(14 + parent.headerSize, parent.pixelTableStart);
+    this.pixelTable = asset.slice(parent.pixelTableStart);
+  }
 
   fs.readFile(path, (err, asset) => {
     if(err) console.error(err);
 
-    let transformArray = ['changeRed', 'changeBlue', 'changeGreen', 'grayscale', 'invert'];
-
-    const Bitmap = function(buffer) {
-      this.type = buffer.toString('utf-8', 0, 2);
-      this.width = buffer.readUInt16LE(22);
-      this.height = buffer.readUInt16LE(18);
-      this.size = buffer.readUInt16LE(2);
-      this.pixelTableStart = buffer.readUInt16LE(10);
-      this.bitsPerPixel = buffer.readUInt16LE(28);
-      this.headerSize = buffer.readUInt16LE(14);
-
-      this.bitsPerPixel < 16 ?
-      this.colorTableStartPoint = this.headerSize + 14 :
-      this.pixelTableStart = this.headerSize + 14 ;
-
-      this.pixelTableEnd = buffer.length;
-      this.colorTableStartPoint ?
-      this.colorTableEndPoint = this.pixelTableStart:
-      null;
-      this.buffer = buffer;
-
-      this.transformedBMP = new TransformedBMP(asset, this);
-
-    };
-
-    function TransformedBMP(asset, parent) {
-      this.header = asset.slice(0, 14);
-      this.DIB = asset.slice(14, 54);
-      if(parent.colorTableStartPoint) this.colorPalette = asset.slice(14 + parent.headerSize, Bitmap.pixelTableStart);
-      this.pixelTable = asset.slice(Bitmap.pixelTableStart);
-    }
+    // let transformArray = ['changeRed', 'changeBlue', 'changeGreen', 'grayscale', 'invert'];
 
     let bitmap = new Bitmap(asset);
 
@@ -51,7 +50,7 @@ module.exports = (path, callback) => {
     let DIBBuf = asset.slice(14, 54);
     let colorTableBuffer = asset.slice(54, Bitmap.pixelTableStart);
     //Same exact value. Delete one of these.
-    let colorTableBuf = asset.slice(54, Bitmap.pixelTableStart);
+    // let colorTableBuf = asset.slice(54, Bitmap.pixelTableStart);
     let pixMapBuf = asset.slice(Bitmap.pixelTableStart);
 
     // this takes the color table from buffer and puts it into an array
@@ -66,7 +65,7 @@ module.exports = (path, callback) => {
         colorTableArr[i] = 255;
         i = i + 4;
       }
-    };
+    }
 
     //grayscale
 
