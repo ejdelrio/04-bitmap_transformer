a1bcf05227d534548f38bdbf6941bd0367466586
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-23 18:36:10 -0700

    Refactored helper function in modify method

--- bitmap-ram-rod/lib/transformer.js
+++ bitmap-ram-rod/lib/transformer.js
@@ -4,14 +4,19 @@ const transform = module.exports = {};
 
 transform.modify = (parent, blueCallback, greenCallback, redCallback) => {
 
-  const helper = function(callback, bmp, ...ind) {
-    return callback(bmp.buffer.readUInt8(ind[0]), bmp.buffer.readUInt8(ind[1]), bmp.buffer.readUInt8(ind[2]));
+  const helper = (bmp, position) => (callback) => (...ind) => {
+    return bmp.writeUInt8(callback(
+      bmp.readUInt8(ind[0]), bmp.readUInt8(ind[1]), bmp.readUInt8(ind[2])),
+    position);
   };
 
-  for (let i = 3 + parent.colorTableStartPoint; i < parent.colorTableEndPoint; i+=4) {
-    parent.buffer.writeUInt8(helper(blueCallback, parent, i-3, i-2, i-1), i - 3);
-    parent.buffer.writeUInt8(helper(greenCallback, parent, i-2, i-3, i-1), i - 2);
-    parent.buffer.writeUInt8(helper(redCallback, parent, i-1, i-2, i-3), i - 1);
+  let start = parent.colorTableStartPoint;
+  let end = parent.colorTableEndPoint;
+  
+  for (let i = 3 + start; i < end; i+=4) {
+    helper(parent.buffer, i - 3)(blueCallback)(i-3, i-2, i-1);
+    helper(parent.buffer, i - 2)(greenCallback)(i-2, i-3, i-1);
+    helper(parent.buffer, i - 1)(redCallback)(i-1, i-2, i-3);
   }
 };
 
