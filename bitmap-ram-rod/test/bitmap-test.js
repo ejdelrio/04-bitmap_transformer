f1681fb091cf8569b34b05a17a387fb12e077564
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-24 16:22:17 -0700

    Rewrote both tests :/

--- bitmap-ram-rod/test/bitmap-test.js
+++ bitmap-ram-rod/test/bitmap-test.js
@@ -6,34 +6,76 @@ const bitmapper = require(`${__dirname}/../model/bitmap.js`);
 
 let path = './assets/palette-bitmap.bmp';
 
-describe('Constructor Test', function() {
-  describe('not an object', function() {
-    it('should return an error', function() {
-      bitmapper.Bitmap();
-      expect(bitmapper.Bitmap).to.be.an('error');
-    });
-  });
-
-  describe('Bitmap object', function() {
-    it('should instantiate an object', function(done) {
-      fs.readFile(path, (err, asset) => {
+describe('Test for Bitmap object Constructor', () => {
+  describe('Object construction', () => {
+    it('Should have throw an err', (done) => {
+      fs.readFile('./assets/not-an-image.bmp', (err) => {
         if(err) console.error(err);
-        let bitmap = new bitmapper.Bitmap(asset);
-        expect(bitmap).to.be.an('object');
+        expect(err).to.be.an('error');
+        done();
       });
-      done();
     });
-    it('should have all Constructor properties', (done) => {
+    it('Should create a new Bitmap object', (done) => {
       fs.readFile(path, (err, asset) => {
-        if(err) console.error(err);
-        let result = new bitmapper.Bitmap(asset);
-        expect(result).to.have.property('type');
-        expect(result).to.have.property('pixleTableStart', 10);
-        expect(result).to.have.property('headerSize', 14);
-        expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
-        expect(result).to.have.property('pixelTableEnd','buffer.length');
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.be.an('object');
+        done();
       });
-      done();
+    });
+    it('Should have the same file type as the asset', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('type', 'BM');
+        expect(testBitmap.type).to.equal(asset.toString('utf-8', 0, 2));
+        done();
+      });
+    });
+    it('Should have the same the same pixel table offset as the asset', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('pixelTableStart', 1078);
+        expect(testBitmap.pixelTableStart).to.equal(asset.readUInt16LE(10));
+        done();
+      });
+    });
+    it('Should have the same bits per pixel as the asset', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('bitsPerPixel', 8);
+        expect(testBitmap.bitsPerPixel).to.equal(asset.readUInt16LE(28));
+        done();
+      });
+    });
+    it('Should have the same header size as the asset', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('headerSize', 40);
+        expect(testBitmap.headerSize).to.equal(asset.readUInt16LE(14));
+        done();
+      });
+    });
+    it('pixelTableEnd should equal the length of the buffer', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('pixelTableEnd', 11078);
+        expect(testBitmap.pixelTableEnd).to.equal(asset.length);
+        done();
+      });
+    });
+    it('It\'s color table should end at the stored offset point', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) return console.error(err);
+        let testBitmap = new bitmapper.Bitmap(asset);
+        expect(testBitmap).to.have.property('colorTableEndPoint', 1078);
+        expect(testBitmap.colorTableEndPoint).to.equal(asset.readUInt16LE(10));
+        done();
+      });
     });
   });
 });
--- bitmap-ram-rod/test/transformer-test.js
+++ bitmap-ram-rod/test/transformer-test.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const expect = require('chai').expect;
 const fs = require('fs');
@@ -7,29 +7,43 @@ const bitmapper = require('./../model/bitmap.js');
 
 let path = './assets/palette-bitmap.bmp';
 
-fs.readFile(path, (err, asset) => {
-  if(err) return console.error(err);
-  let bitBuffer = new bitmapper.Bitmap(asset);
-  let header = asset.toString('hex', 0, 14);
-  let DIB = asset.toString('hex', 14, 54);
-  let colorPallete = asset.toString('hex', 54, 1078);
-  let pixelArray = asset.toString('hex', 1078, asset.length);
-
-  transform.modify(bitBuffer, () => 0, () => 0, () => 0);
-  
-  describe('Transformer Module Tests', () => {
-    describe('#modify', () => {
-      it('The file header should remain the same', () => {
+describe('Transformer Module Tests', () => {
+  describe('#modify', () => {
+    it('The file header should remain the same', (done) => {
+      fs.readFile(path, (err, asset) => {
+        let testBitmap = new bitmapper.Bitmap(asset);
+        let header = asset.toString('hex', 0, 14);
+        transform.modify(testBitmap, () => 0, () => 0, () => 0);
         expect(asset.toString('hex', 0, 14)).to.equal(header);
+        done();
       });
-      it('The DIB header should remain the same', () => {
+
+    });
+    it('The DIB header should remain the same', (done) => {
+      fs.readFile(path, (err, asset) => {
+        let testBitmap = new bitmapper.Bitmap(asset);
+        let DIB = asset.toString('hex', 14, 54);
+        transform.modify(testBitmap, () => 0, () => 0, () => 0);
         expect(asset.toString('hex', 14, 54)).to.equal(DIB);
+        done();
       });
-      it('The color palette should be modified', () => {
+    });
+    it('The color palette should be modified', (done) => {
+      fs.readFile(path, (err, asset) => {
+        let testBitmap = new bitmapper.Bitmap(asset);
+        let colorPallete = asset.toString('hex', 54, 1078);
+        transform.modify(testBitmap, () => 0, () => 0, () => 0);
         expect(asset.toString('hex', 54, 1078)).not.to.equal(colorPallete);
+        done();
       });
-      it('The pixel array should remain the same', () => {
+    });
+    it('The pixel array should remain the same', (done) => {
+      fs.readFile(path, (err, asset) => {
+        let testBitmap = new bitmapper.Bitmap(asset);
+        let pixelArray = asset.toString('hex', 1078, asset.length);
+        transform.modify(testBitmap, () => 0, () => 0, () => 0);
         expect(asset.toString('hex', 1078, asset.length)).to.equal(pixelArray);
+        done();
       });
     });
   });
