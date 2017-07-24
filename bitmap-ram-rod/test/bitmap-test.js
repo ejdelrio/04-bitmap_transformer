48f13b7a70916667299455cdf0d5cd203bcece47
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-24 13:05:41 -0700

    refactored it block to object to be multiple it blocks

--- bitmap-ram-rod/test/bitmap-test.js
+++ bitmap-ram-rod/test/bitmap-test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const bitmapper = require(`${__dirname}/../model/bitmap.js`);
 
-let path = '../assets/palette-bitmap.bmp';
+let path = `${__dirname}/../assets/palette-bitmap.bmp`;
 
 describe('Constructor Test', function() {
   describe('not an object', function() {
@@ -15,24 +15,60 @@ describe('Constructor Test', function() {
     });
   });
 
-  describe('Bitmap object', function() {
-    it('should instantiate an object', function(done) {
-      fs.readFile(path, (err, asset) => {
-        if(err) console.error(err);
-        let bitmap = new bitmapper.Bitmap(asset);
-        expect(bitmap).to.be.an('object');
+  describe('bitmap object', function() {
+    it('should instantiate an object', function() {
+      bitmapper(path, (err, data) => {
+        expect(err).to.equal(null);
+        expect(data).to.be.an('object');
         done();
       });
     });
-    it('should have all Constructor properties', (done) => {
-      fs.readFile(path, (err, asset) => {
-        if(err) console.error(err);
-        let result = new bitmapper.Bitmap(asset);
-        expect(result).to.have.property('type');
-        expect(result).to.have.property('pixleTableStart', 10);
-        expect(result).to.have.property('headerSize', 14);
-        expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
-        expect(result).to.have.property('pixelTableEnd','buffer.length');
+    it('should have a key type with a value of BM', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('type', 'BM');
+        done();
+      });
+    });
+    it('should have a key pixelTableStart with a value of 1078', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('pixelTableStart', 1078);
+        done();
+      });
+    });
+    it('should have a key bitsPerPixel with a value of 8', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('bitsPerPixel', 8);
+        done();
+      });
+    });
+    it('should have a key headerSize with a value of 40', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('headerSize', 40);
+        done();
+      });
+    });
+    it('should have a key colorTableStartPoint with a value of 54', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('colorTableStartPoint', 54);
+        done();
+      });
+    });
+    it('should have a key pixelTableEnd with a value of 11078', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('pixelTableEnd', 11078);
+        done();
+      });
+    });
+    it('should have a key colorTableEndPoint with a value of 1078', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('colorTableEndPoint', 1078);
         done();
       });
     });
--- bitmap-ram-rod/test/transformer-test.js
+++ bitmap-ram-rod/test/transformer-test.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 const transform = require('./../lib/transformer.js');
 const bitmapper = require('./../model/bitmap.js');
 
-let path = './assets/palette-bitmap.bmp';
-let newPath = './asset/'
+let path = `${__dirname}/../assets/palette-bitmap.bmp`;
 
 describe('Transformer Module Tests', () => {
   describe('#modify', () => {
@@ -23,3 +22,15 @@ describe('Transformer Module Tests', () => {
     });
   });
 });
+
+// let header = originalAsset.toString(0, 14);
+//     let DIB = originalAsset.toString(14, 54);
+//     let colorPalette = originalAsset.toString(54, 1078);
+//     let pixelArray = originalAsset.toString(1078, originalAsset);
+//
+//     transform.modify(originalAsset, () => 0, () => 0, () => 0);
+//     expect(originalAsset.toString(0, 14)).to.equal(header);
+//     expect(originalAsset.toString(14, 54)).to.equal(DIB);
+//     expect(originalAsset.toString(54, 1078)).not.to.equal(DIB);
+//     expect(originalAsset.toString(1078, originalAsset.length)).to.equal(DIB);
+//     done();
