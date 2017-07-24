56cdca5be589c1fe8c55314374433d8e9e1e46a8
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-23 23:18:22 -0700

    Refactored test

--- bitmap-ram-rod/test/bitmap-test.js
+++ bitmap-ram-rod/test/bitmap-test.js
@@ -2,64 +2,39 @@
 
 const fs = require('fs');
 const expect = require('chai').expect;
-const Bitmap = require(`${__dirname}/../model/bitmap.js`);
+const bitmapper = require(`${__dirname}/../model/bitmap.js`);
+
+let path = '../assets/palette-bitmap.bmp';
 
 describe('Constructor Test', function() {
   describe('not an object', function() {
     it('should return an error', function(done) {
-      let result = Bitmap();
+      let result = bitmapper.Bitmap();
       expect(err).to.be.an('error');
       done();
       });
     });
 
   describe('Bitmap object', function() {
-    it('should instantiate an object', function() {
-      let result = Bitmap();
-      expect(result).to.be.an('object');
+    it('should instantiate an object', function(done) {
+      fs.readFile(path, (err, asset) => {
+        if(err) console.error(err);
+        let bitmap = new bitmapper.Bitmap(asset);
+        expect(bitmap).to.be.an('object');
+      });
+      done();
     });
-    it('should have all Constructor properties' () => {
-      let result = Bitmap();
-      expect(result).to.have.property('type');
-      expect(result).to.have.property('pixleTableStart', 10);
-      expect(result).to.have.property('headerSize', 14);
-      expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
-      expect(result).to.have.property('pixelTableEnd','buffer.length');
-    })
-  });
-
-  describe('if a type', function() {
-    it('should be a type', function() {
-      let result = Bitmap();
-      expect(result).to.have.property('type');
-    });
-  });
-
-  describe('if a pixel table', function() {
-    it('should be pixel table', function() {
-      let result = Bitmap();
-      expect(result).to.have.property('pixleTableStart', 10);
-    });
-  });
-
-  describe('if a headerSize', function() {
-    it('should be a headerSize', function() {
-      let result = Bitmap();
-      expect(result).to.have.property('headerSize', 14);
-    });
-  });
-
-  describe('if a colorTable', function() {
-    it('should be a colorTable', function() {
-      let result = Bitmap();
-      expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
-    });
-  });
-
-  describe('if a pixelTableEnd', function() {
-    it('should be a pixelTableEnd', function() {
-      let result = Bitmap();
-      expect(result).to.have.property('pixelTableEnd','buffer.length');
+    it('should have all Constructor properties', (done) => {
+      fs.readFile(path, (err, asset) => {
+        if(err) console.error(err);
+        let result = new bitmapper.Bitmap(asset);
+        expect(result).to.have.property('type');
+        expect(result).to.have.property('pixleTableStart', 10);
+        expect(result).to.have.property('headerSize', 14);
+        expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
+        expect(result).to.have.property('pixelTableEnd','buffer.length');
+      });
+      done();
     });
   });
 });
