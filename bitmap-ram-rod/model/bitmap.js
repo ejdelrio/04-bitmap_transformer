617ea592212a75102ae08bab4e1a49adf5c2978a
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-24 00:09:37 -0700

    fixed testing issues

--- bitmap-ram-rod/model/bitmap.js
+++ bitmap-ram-rod/model/bitmap.js
@@ -1,47 +1,45 @@
-
-'use strict';
+'use srict';
 
 const fs = require('fs');
 const transform = require('../lib/transformer.js');
 
 
-const bitmapper = module.exports = {};
+module.exports = (path, callback, newFileName) => {
 
+  const Bitmap = function(buffer) {
+    this.type = buffer.toString('utf-8', 0, 2);
+    this.pixelTableStart = buffer.readUInt16LE(10);
+    this.bitsPerPixel = buffer.readUInt16LE(28);
+    this.headerSize = buffer.readUInt16LE(14);
 
-function Bitmap(buffer) {
-  this.type = buffer.toString('utf-8', 0, 2);
-  this.pixelTableStart = buffer.readUInt16LE(10);
-  this.bitsPerPixel = buffer.readUInt16LE(28);
-  this.headerSize = buffer.readUInt16LE(14);
+    this.bitsPerPixel < 16 ?
+    this.colorTableStartPoint = this.headerSize + 14 :
+    this.pixelTableStart = this.headerSize + 14 ;
 
-  this.bitsPerPixel < 16 ?
-  this.colorTableStartPoint = this.headerSize + 14 :
-  this.pixelTableStart = this.headerSize + 14 ;
+    this.pixelTableEnd = buffer.length;
 
-  this.pixelTableEnd = buffer.length;
+    this.colorTableStartPoint ?
+    this.colorTableEndPoint = this.pixelTableStart:
+    null;
+    this.buffer = buffer;
 
-  this.colorTableStartPoint ?
-  this.colorTableEndPoint = this.pixelTableStart:
-  null;
-  this.buffer = buffer;
-}
+  };
 
-Bitmap.prototype.newFile = function(fileName) {
-  let newBuffer = this.buffer;
-  fs.writeFile(`./assets/${fileName}.bmp`, newBuffer, (err) => {
-    if(err) console.error(err);
-    console.log(`${fileName}.bmp created!!`);
-  });
-};
+  Bitmap.prototype.newFile = function(fileName) {
+    let newBuffer = this.buffer;
+    fs.writeFile(`./assets/${fileName}.bmp`, newBuffer, (err) => {
+      if(err) console.error(err);
+      console.log(`${fileName}.bmp created!!`);
+    });
+  };
 
-bitmapper.Bitmap = Bitmap;
-
-
-bitmapper.renderImage = function(path, callback, newFileName) {
   fs.readFile(path, (err, asset) => {
     if(err) console.error(err);
-    let bitmap = new bitmapper.Bitmap(asset);
+
+    let bitmap = new Bitmap(asset);
     callback(bitmap);
     bitmap.newFile(newFileName);
   });
+
+
 };
--- bitmap-ram-rod/test/bitmap-test.js
+++ bitmap-ram-rod/test/bitmap-test.js
@@ -4,36 +4,37 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const bitmapper = require(`${__dirname}/../model/bitmap.js`);
 
-let path = '../assets/palette-bitmap.bmp';
+let path = `${__dirname}/../assets/palette-bitmap.bmp`;
 
 describe('Constructor Test', function() {
   describe('not an object', function() {
-    it('should return an error', function() {
-      bitmapper.Bitmap();
-      expect(bitmapper.Bitmap()).to.be.an('error');
+    it('should return an error', function(done) {
+      let result = bitmapper;
+      expect(result).to.throw(Error);
+      done();
     });
   });
 
-  describe('Bitmap object', function() {
-    it('should instantiate an object', function(done) {
-      fs.readFile(path, (err, asset) => {
-        if(err) console.error(err);
-        let bitmap = new bitmapper.Bitmap(asset);
-        expect(bitmap).to.be.an('object');
+  describe('bitmap object', function() {
+    it('should instantiate an object', function() {
+      bitmapper(path, (err, data) => {
+        expect(err).to.equal(null);
+        expect(data).to.be.an('object');
         done();
       });
     });
-    it('should have all Constructor properties', (done) => {
-      fs.readFile(path, (err, asset) => {
-        if(err) console.error(err);
-        let result = new bitmapper.Bitmap(asset);
-        expect(result).to.have.property('type');
-        expect(result).to.have.property('pixleTableStart', 10);
-        expect(result).to.have.property('headerSize', 14);
-        expect(result).to.have.property('colorTableStartPoint', 'this.header' + 14);
-        expect(result).to.have.property('pixelTableEnd','buffer.length');
+    it('should have all Constructor properties', () => {
+      bitmapper(path, (err,data) =>{
+        expect(err).to.equal(null);
+        expect(data).to.have.property('type', 'BM');
+        expect(data).to.have.property('pixelTableStart', 1078);
+        expect(data).to.have.property('bitsPerPixel', 8);
+        expect(data).to.have.property('headerSize', 40);
+        expect(data).to.have.property('colorTableStartPoint', 54);
+        expect(data).to.have.property('pixelTableEnd', 11078);
+        expect(data).to.have.property('colorTableEndPoint', 1078);
         done();
-      });
-    });
+      })
+    })
   });
 });
