bceb6d4b27b2620250d040fcfcb91fd2977b1868
Loaye <jr.carstens00@gmail.com>
2017-07-23 18:24:40 -0700

    Creates: Bitmap Constructor Test

---
+++ bitmap-ram-rod/test/bitmap-test.js
@@ -0,0 +1,22 @@
+'use strict' ;
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const Bitmap = require(`${__dirname}/../model/bitmap.js`);
+
+describe('Constructor Test', function() {
+  describe('not an object', function() {
+    it('should return an error', function(done) {
+      let result = bitmapConstructor();
+      expect(err).to.be.an('error');
+      done();
+      });
+    });
+  
+    describe('if an object', function() {
+      it('Instantiate Object', function() {
+        let result = bitmapConstructor();
+        expect(result).to.be.an('object');
+    });
+  });
+});
